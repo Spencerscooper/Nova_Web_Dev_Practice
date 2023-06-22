1778b4988818ff118d477f5f33ce037f932a42f5
Spencerscooper <spencercooper1991@gmail.com>
2023-06-22 10:00:32 +0000

    calling function from another fuct

---
+++ Questions_Section/questions.py
@@ -0,0 +1,16 @@
+def message () :
+    print("If you are highly interesting in coding enroll now!!")
+    
+
+def introduction () :
+    message ()
+    print("Welcome to Novaweb Development")
+    print("At Novaweb we believe in helping people learn how to code")
+
+introduction ()
+
+
+# My Question: Since a function can be called by another function as illustrated
+# above in the first stage of my function definition, why is this rules not
+# applying to the function while trying to call the seond function in the first
+# function?
